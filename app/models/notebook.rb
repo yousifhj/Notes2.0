60684639d6ba83f9a5ad82801bd2bea84cd480c0
Huda Yousif <hjoski@gmail.com>
2020-09-30 21:50:57 -0700

    finalize models

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,4 +1,6 @@
 class Notebook < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has many :users 
+  has_many :users though :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :notebooks
+    has_many :comments
+    has_many :commented_notebooks, through :comments,
+        source: notebook
 end
