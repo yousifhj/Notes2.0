0484bbd92edeb8252c639eb09817c988210a2627
Huda Yousif <hjoski@gmail.com>
2020-10-23 16:40:58 -0700

    created create action in notbook controller

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -4,4 +4,20 @@ class NotebooksController < ApplicationController
     def new 
         @notebook = Notebook.new 
     end 
+
+    def create
+        @notebook = current_user.notebooks.build(notebook_params)
+        if @notebook.save
+          redirect_to notebooks_path
+        else
+          render :new
+        end
+      end
+
+    private 
+
+    def notebook_params 
+        params.require(:notebook).permit(:title, :content)
+    end 
+
 end
