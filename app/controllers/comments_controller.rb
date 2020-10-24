44a0db0a61193b958c19b8bae02507da6226a24c
Huda Yousif <hjoski@gmail.com>
2020-10-24 12:05:26 -0700

    start comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+    def new 
+        @comments = Comments.new 
+    end 
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+          redirect_to comments_path
+        else
+          render :new
+        end
+    end
+
+    def show 
+    end 
+
+    def edit
+    end 
+
+
 end
